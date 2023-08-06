a6fb7b27fa73262db210afad963edf042a1bebf4
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-06 18:16:38 -0400

    update lib

--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -5,3 +5,10 @@ from bool_functions import return_true
 def test_return_true():
     '''in bool_functions, function "return_true" returns True.'''
     assert return_true() == True
+
+# testing/subdirectory/bool_test.py
+
+ # testing/subdirectory/bool_functions.py
+
+def return_true():
+    return True
--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -5,3 +5,17 @@ from not_none_functions import return_not_none
 # def test_return_not_none():
 #     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
 #     assert False
+# testing/test_not_none.py
+
+ # testing/test_not_none.py
+
+from not_none_functions import return_not_none
+
+def test_return_not_none():
+    result = return_not_none()
+    assert result is not None, "Function should not return None"
+    assert isinstance(result, str), "Function should return a string"
+# testing/not_none_functions.py
+
+def return_not_none():
+    return "Hello, this is not None!"
